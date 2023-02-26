1c5a54fe0115cbdd969ae9924b4042cdf296b221
marissanura <icamarissa24@gmail.com>
2023-02-26 18:04:19 +0700

    :hammer: Update Variable

--- Prototype/use_case_csv_reader.py
+++ Prototype/use_case_csv_reader.py
@@ -4,17 +4,18 @@ from csv_reader import CSVReader
 from xml.etree.ElementTree import ( Element, SubElement )
 import re
 
-
 class CSVReader(CSVReader):
-    fh = ["SS-[0-9]{1,}\Z","UC-[0-9]{1,}\Z","\A\Z","UC-[0-9]{1,}-[0-9]{1,}\Z"]
 
-    def generate_tree(self):
-        self.num_of_fh = len(self.fh)
-        self.num_of_line = len(self.reader) - 1
+    def generate_tree(self, folder_hierarchy):
+        global num_of_fh, num_of_line, list_of_node, fh
+        
+        fh = folder_hierarchy
+        num_of_fh = len(fh)
+        num_of_line = len(self.reader) - 1
         self.root = Element('UseCaseDiagram')
 
-        self.list_of_node = []
-        self.list_of_node.append(self.root)
+        list_of_node = []
+        list_of_node.append(self.root)
         self.generate_use_case(0,1)
 
         xmlstr = minidom.parseString(ElementTree.tostring(self.root)).toprettyxml(indent="   ") 
@@ -24,16 +25,16 @@ class CSVReader(CSVReader):
         return self.root
     
     def generate_use_case(self, fol_hierarchy, lines):
-        if re.match(self.fh[fol_hierarchy], self.reader[lines][0]):
-            temp_node = self.add_element(self.list_of_node[fol_hierarchy], self.reader[lines])
-            if (fol_hierarchy != (self.num_of_fh - 1)): self.add_line_of_node(temp_node, fol_hierarchy) 
-            if lines < self.num_of_line:
+        if re.match(fh[fol_hierarchy], self.reader[lines][0]):
+            temp_node = self.add_element(list_of_node[fol_hierarchy], self.reader[lines])
+            if (fol_hierarchy != (num_of_fh - 1)): self.add_line_of_node(temp_node, fol_hierarchy) 
+            if lines < num_of_line:
                 lines = lines + 1
                 self.generate_use_case(fol_hierarchy, lines)
             else:
                 return True
         else:
-            fol_hierarchy = 0 if (fol_hierarchy == (self.num_of_fh - 1)) else (fol_hierarchy + 1)
+            fol_hierarchy = 0 if (fol_hierarchy == (num_of_fh - 1)) else (fol_hierarchy + 1)
             self.generate_use_case(fol_hierarchy, lines)
     
     def add_element(self, parent, data):
@@ -47,6 +48,6 @@ class CSVReader(CSVReader):
         return temp
     
     def add_line_of_node(self, temp_node, fol_hierarchy):
-        if ((len(self.list_of_node)-1) > fol_hierarchy):
-            self.list_of_node[fol_hierarchy + 1] = temp_node
-        else: (self.list_of_node.append(temp_node))
+        if ((len(list_of_node)-1) > fol_hierarchy):
+            list_of_node[fol_hierarchy + 1] = temp_node
+        else: (list_of_node.append(temp_node))
--- Prototype/use_case_generator.py
+++ Prototype/use_case_generator.py
@@ -4,5 +4,6 @@ from sparx_ea_xml_writer import SparxEAXMLWriter
 
 class UseCaseGenerator:
     datacsv = CSVReader("CSV-ok.csv")
-    SparxEAXMLWriter(datacsv.generate_tree()).write('output.xml')
+    folder_hierarcy = ["SS-[0-9]{1,}\Z","UC-[0-9]{1,}\Z","\A\Z","UC-[0-9]{1,}-[0-9]{1,}\Z"]
+    SparxEAXMLWriter(datacsv.generate_tree(folder_hierarcy)).write('output.xml')
     
